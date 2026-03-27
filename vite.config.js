2ea1a8451f861b135272013d41f18477aaa933ad
pramiti-dishalini-dharmadevan <dishalini.dharmadevan@pramiti.co.in>
2026-03-27 10:44:32 +0530

    upload files

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,16 @@ export default defineConfig({
   plugins: [react()],
   server: {
     open: true
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        projects: resolve(__dirname, 'projects.html'),
+        blog: resolve(__dirname, 'blog.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        // add all your pages here
+      }
+    }
   }
 });
